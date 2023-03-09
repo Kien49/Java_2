6d3d30244e2e1d4c5e09931ae3b42df229e776c7
Students <kieukiena1y2k@gmail.com>
2023-03-09 15:42:56 +0700

    day1

--- src/main/java/Day1b/Bai2/MainB2.java
+++ src/main/java/Day1b/Bai2/MainB2.java
@@ -1,5 +1,22 @@
 package Day1b.Bai2;
 
 public class MainB2 {
-    //ITinhToan<SoPhuc>
+    public static void main(String[] args) {
+        ITinhToan<SoPhuc> soPhuc1 = new TinhToanSoPhuc();
+
+        SoPhuc sp1 = new SoPhuc(2, 3);
+        SoPhuc sp2 = new SoPhuc(4, 5);
+        SoPhuc sp3 = new SoPhuc(6, 7);
+
+        SoPhuc t1 = soPhuc1.cong(sp1, sp2, sp3);
+        SoPhuc t2 = soPhuc1.tru(sp1 , sp2);
+        SoPhuc t3 = soPhuc1.nhan(sp2 , sp3);
+        SoPhuc t4 = soPhuc1.chia(sp1, sp3);
+
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+        System.out.println(t4);
+    }
+
 }
